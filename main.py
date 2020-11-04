c92c2f4bc6ff3c5870e313b7356c05c194482e57
sota.m <masuda.kyushu@gmail.com>
2020-11-05 00:42:11 +0900

    Add spaCy and spaCy test code.

---
+++ main.py
@@ -0,0 +1,18 @@
+"""word embedding core
+    - easiest: using spaCy trained model
+    - harder but better: train custom model with spaCy or gensim
+"""
+
+import spacy
+
+
+def main():
+    nlp = spacy.load('ja_core_news_lg')
+    doc = nlp('わたしは山田太郎ですよ。')
+    for token in doc:
+        print(token.i, ', ', token.text, ', ', token.pos_)
+
+
+if __name__ == "__main__":
+    main()
+
