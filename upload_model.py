9a29c9debc1389a14cc0ecc46aacf018b0201953
sota.m <masuda.kyushu@gmail.com>
2020-11-18 14:59:24 +0900

    update uploading model and data.

--- upload_model.py
+++ upload_model.py
@@ -1,19 +1,23 @@
 """Upload model and user's vector files to Cloud Storage
 """
 from pathlib import Path
+import os
 import sys
 
 from google.cloud import storage
 
 
 def create_bucket_class_location(
-    bucket_name: str, storage_class: str='STANDARD', location: str='asia-northeast1'):
+    bucket_name: str, project_id: str, storage_class: str='STANDARD', location: str='asia-northeast1'):
     """Create a new bucket in specific location with storage class"""
     # bucket_name = "your-new-bucket-name"
 
     storage_client = storage.Client()
 
     bucket = storage_client.bucket(bucket_name)
+    if bucket.exists(storage_client):
+        print('bucket has already existed.')
+        return
     bucket.storage_class = storage_class
     new_bucket = storage_client.create_bucket(bucket, location=location)
 
@@ -48,21 +52,33 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
 def main():
     """Main proc
     """
+    bucket_name = os.environ['GAE_DEFAULT_BUCKET_NAME']
+    project_id = os.environ['BQ_PROJECT_ID']
+    create_bucket_class_location(bucket_name, project_id)
+    
     p = Path('./data')
     
-    p_list = list(p.glob('*.model'))
+    # upload trained model
+    p_list = list(p.glob('*.model.pkl'))
     p_model = p_list[0] if len(p_list) > 0 else None
     if p_model is None:
         print('model path is invalid.')
         sys.exit(1)
+    upload_blob(bucket_name, str(p_model), p_model.name)
+
+    # upload users table
+    p_list = list(p.glob('users.csv'))
+    p_users_tbl = p_list[0] if len(p_list) > 0 else None
+    if p_users_tbl is None:
+        print('users tbl path is invalid.')
+        sys.exit(1)
+    upload_blob(bucket_name, str(p_users_tbl), p_users_tbl.name) 
+
+    # upload user's vectors
     p_list = list(p.glob('*.json'))
     p_list = [x for x in p_list if x.stem not in ['dataset', 'test_dataset', 'train_dataset']]
     p_vectors = p_list
 
-    bucket_name = 'dlg-doc2vec-modules'
-    create_bucket_class_location(bucket_name)
-
-    upload_blob(bucket_name, str(p_model), p_model.name)
     for p_vector in p_vectors:
         upload_blob(bucket_name, str(p_vector), 'vectors/' + p_vector.name)
 
